3f317ddbcd25da58d54cec925be3b5ca68d93f71
Kano.M <itspapari888@gmail.com>
2024-06-18 06:51:17 +0900

    add:ログイン、会員登録共通レイアウト

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/layouts/app.css') }}">
+    @yield('css')
+</head>
+<body>
+    <header class="header">
+            <img src="{{ asset('images/logo.svg') }}" alt="COACHTECHロゴ" class="logo">
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
