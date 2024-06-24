1945e7123272675c486eac141288c84946bf89e5
Kano.M <itspapari888@gmail.com>
2024-06-25 02:33:04 +0900

    feat: マイページに出品商品一覧を表示するための処理を追加

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Item;
 
 class UserController extends Controller
 {
     public function mypage()
     {
-        return view('mypage');
+        $user = Auth::user();
+        $items = Item::where('user_id', $user->id)->get();
+        return view('mypage', compact('user', 'items'));
     }
 }
