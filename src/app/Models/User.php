35b344b0cd39a389cc37cda786089e95c0aed9f0
Kano.M <itspapari888@gmail.com>
2024-06-30 01:44:48 +0900

    feat: favoritesテーブル、Favoriteモデル、リレーション追加

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Item::class);
     }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
 }
