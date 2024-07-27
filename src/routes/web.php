eb47411d9b1afb781f5b7a2e41b0c083523bde1e
Kano.M <itspapari888@gmail.com>
2024-07-27 23:47:06 +0900

    refactor: web.phpのルート定義をリファクタリングし、プレフィックスを適用

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\FavoriteController;
-use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\AdminAuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SearchController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,20 +37,23 @@ Route::get('/', function () {
 
 Route::controller(ItemController::class)->group(function () {
     Route::get('/', 'index')->name('home');
-    Route::get('/item/{id}', 'show')->name('item.show');
-    Route::get('/item/{id}/purchase', 'purchase')->name('item.purchase');
-    Route::get('/item/{id}/address', 'address')->name('address');
-    Route::post('/item/{id}/address/update', 'updateAddress')->name('address.update');
-    Route::post('/item/{id}/confirm-purchase', 'confirmPurchase')->name('item.confirm-purchase');
+    Route::prefix('item')->group(function () {
+        Route::get('/{id}', 'show')->name('item.show');
+        Route::get('/{id}/purchase', 'purchase')->name('item.purchase');
+        Route::get('/{id}/address', 'address')->name('address');
+        Route::post('/{id}/address/update', 'updateAddress')->name('address.update');
+        Route::post('/{id}/confirm-purchase', 'confirmPurchase')->name('item.confirm-purchase');
+    });
     Route::get('/create', 'create')->name('create');
     Route::post('/create', 'store')->name('item.store');
     Route::get('/complete', 'complete')->name('item.complete');
 });
 
-Route::controller(CommentController::class)->group(function () {
-    Route::get('/item/{id}/comments', 'showComments')->name('comments.show');
-    Route::post('/item/{id}/comments', 'submitComment')->name('comments.submit');
-    Route::delete('/item/{item}/comments/{comment}', 'deleteComment')->name('comments.delete');
+
+Route::prefix('item')->controller(CommentController::class)->group(function () {
+    Route::get('/{id}/comments', 'showComments')->name('comments.show');
+    Route::post('/{id}/comments', 'submitComment')->name('comments.submit');
+    Route::delete('/{item}/comments/{comment}', 'deleteComment')->name('comments.delete');
 });
 
 Route::get('/mypage', [UserController::class, 'mypage'])->name('mypage');
@@ -60,15 +63,15 @@ Route::middleware('auth')->group(function () {
     Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
 });
 
-Route::controller(FavoriteController::class)->group(function () {
-    Route::post('/favorite/{item}', 'toggleFavorite')->name('favorite.toggle');
-    Route::delete('/favorite/{item}', 'destroy')->name('favorite.destroy');
+Route::prefix('favorite')->controller(FavoriteController::class)->group(function () {
+    Route::post('/{item}', 'toggleFavorite')->name('favorite.toggle');
+    Route::delete('/{item}', 'destroy')->name('favorite.destroy');
 });
 
-Route::controller(PaymentController::class)->group(function () {
-    Route::get('/item/{id}/change-payment', 'changePaymentMethod')->name('payment.change');
-    Route::post('/item/{id}/update-payment', 'updatePaymentMethod')->name('payment.update');
-    Route::post('/item/{id}/complete-payment', 'completePayment')->name('payment.complete');
+Route::prefix('item/{id}')->controller(PaymentController::class)->group(function () {
+    Route::get('/change-payment', 'changePaymentMethod')->name('payment.change');
+    Route::post('/update-payment', 'updatePaymentMethod')->name('payment.update');
+    Route::post('/complete-payment', 'completePayment')->name('payment.complete');
 });
 
 Route::get('/payment/sent', function () {
@@ -76,20 +79,22 @@ Route::get('/payment/sent', function () {
 })->name('payment.sent');
 
 Route::prefix('admin')->group(function () {
-    Route::get('login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
-    Route::post('login', [AdminAuthController::class, 'login'])->name('admin.login');
-    Route::post('logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+    Route::controller(AdminAuthController::class)->group(function () {
+        Route::get('login', 'showLoginForm')->name('admin.login');
+        Route::post('login', 'login');
+        Route::post('logout', 'logout')->name('admin.logout');
+    });
 
-    Route::middleware('auth:admin')->group(function () {
-        Route::get('users', [AdminController::class, 'index'])->name('admin.users');
-        Route::get('users/{id}', [AdminController::class, 'show'])->name('admin.users.show');
-        Route::delete('users/{id}', [AdminController::class, 'deleteUser'])->name('admin.users.delete');
-        Route::delete('comments/{id}', [AdminController::class, 'deleteComment'])->name('admin.comments.delete');
-        Route::post('users/{id}/send-email', [AdminController::class, 'sendEmail'])->name('admin.users.send-email');
+    Route::middleware('auth:admin')->controller(AdminController::class)->group(function () {
+        Route::get('users', 'index')->name('admin.users');
+        Route::get('users/{id}', 'show')->name('admin.users.show');
+        Route::delete('users/{id}', 'deleteUser')->name('admin.users.delete');
+        Route::delete('comments/{id}', 'deleteComment')->name('admin.comments.delete');
+        Route::post('users/{id}/send-email', 'sendEmail')->name('admin.users.send-email');
     });
 });
 
-Route::controller(SearchController::class)->prefix('search')->group(function () {
+Route::prefix('search')->controller(SearchController::class)->group(function () {
     Route::get('/', 'search')->name('items.search');
     Route::get('/category', 'searchByCategory')->name('items.search.category');
     Route::get('/condition', 'searchByCondition')->name('items.search.condition');
