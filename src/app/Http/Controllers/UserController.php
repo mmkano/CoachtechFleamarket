be1d23815caac4e91cd4ac107603f0267fddacd1
Kano.M <itspapari888@gmail.com>
2024-07-27 23:48:05 +0900

    refactor: コントローラーのuseステートメントをアルファベット順に並べ替え

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Models\Item;
 use App\Models\SoldItem;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
